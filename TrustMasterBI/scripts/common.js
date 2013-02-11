81d4e99ff4d04703df2db9d3784638e3fb87e134
ashu.khandelwal30@gmail.com <ashu.khandelwal30@gmail.com>
2013-02-11 04:38:00 -0600

    css and js function added

---
+++ TrustMasterBI/scripts/common.js
@@ -0,0 +1,12 @@
+function createCSS(){
+    document.write('<link rel="stylesheet" href="../kendo/styles/kendo.mobile.all.min.css"/>');
+    document.write('<link href="../kendo/styles/kendo.common.css" rel="stylesheet" />');
+     document.write('<link href="../kendo/styles/kendo.default.css" rel="stylesheet" />');
+}
+
+function createJS(){
+    document.write('<script src="../cordova.js" type="text/javascript"</script>');
+    document.write('<script src="../kendo/js/jquery.min.js"></script>');
+    document.write('<script src="../kendo/js/kendo.mobile.min.js" type="text/javascript"</script>');
+     document.write('<script src="../kendo/js/kendo.all.js" type="text/javascript"</script>');               
+}
