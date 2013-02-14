42d4b70a01cb125f7aed4985a3a449b9c2518aed
partheshdmehta@gmail.com <partheshdmehta@gmail.com>
2013-02-13 23:08:36 -0600

    add call service method in js

--- TrustMasterBI/scripts/common.js
+++ TrustMasterBI/scripts/common.js
@@ -10,3 +10,32 @@ function AddJsOnPage() {
 	document.write('<script src="../kendo/js/kendo.mobile.min.js" type="text/javascript"></script>');
 	document.write('<script src="../kendo/js/kendo.all.js" type="text/javascript"></script>');               
 }
+
+function CallWebService(controller, method, parameter){
+    var url="http://onit1.homenet.org/TrustMasterMobileWebService/" + controller + "/" + method;
+    alert(url);
+    
+    try {
+        $.ajax({
+            "url": url,
+            "method": "post",
+			"dataType": "json"
+			//"data": "arg1=val1&arg2=val2" // chart-specific arguments
+			})
+			.done(function (data, textStatus, jqXHR) {
+							try {
+								data = eval(data)[0];
+                                return data;
+							}
+							catch (e) {
+								alert("Server sent incorrect data. Please contact On-IT1.");
+							}
+						})
+						.fail(function (jqXHR, textStatus, errorThrown) {
+							alert("Server error. Please contact On-IT1.\n\n" + errorThrown);
+						})
+	}
+    catch (e) {
+	    alert("Error occurred. Please contact On-IT1.\n\n" + e);
+    }
+}
