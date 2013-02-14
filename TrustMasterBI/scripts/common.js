d803c53276a2df1a9f0f2af8389c0e68f1088422
partheshdmehta@gmail.com <partheshdmehta@gmail.com>
2013-02-14 01:32:28 -0600

    add web method and call it in graph and tabular page

--- TrustMasterBI/scripts/common.js
+++ TrustMasterBI/scripts/common.js
@@ -11,31 +11,26 @@ function AddJsOnPage() {
 	document.write('<script src="../kendo/js/kendo.all.js" type="text/javascript"></script>');               
 }
 
-function CallWebService(controller, method, parameter){
-    var url="http://onit1.homenet.org/TrustMasterMobileWebService/" + controller + "/" + method;
-    alert(url);
-    
-    try {
-        $.ajax({
-            "url": url,
-            "method": "post",
-			"dataType": "json"
-			//"data": "arg1=val1&arg2=val2" // chart-specific arguments
-			})
-			.done(function (data, textStatus, jqXHR) {
-							try {
-								data = eval(data)[0];
-                                return data;
-							}
-							catch (e) {
-								alert("Server sent incorrect data. Please contact On-IT1.");
-							}
-						})
-						.fail(function (jqXHR, textStatus, errorThrown) {
-							alert("Server error. Please contact On-IT1.\n\n" + errorThrown);
-						})
+function callwebservice(controller, method, parameter) {
+	var url = "http://onit1.homenet.org/TrustMasterMobileWebService/" + controller + "/" + method;
+    var receivedData;
+	try {
+		$.ajax({
+			url: url,
+			cache: false,
+			dataType: 'json',
+            data:parameter,
+			async: false,
+			success: function (data) {
+                receivedData=eval(data)[0];
+			},
+			error: function (msg) {
+				alert("Server error. Please contact On-IT1.\n\n" + errorThrown);
+			}
+		});
+        return receivedData;
 	}
-    catch (e) {
-	    alert("Error occurred. Please contact On-IT1.\n\n" + e);
-    }
+	catch (e) {
+		alert("Error occurred. Please contact On-IT1.\n\n" + e);
+	}
 }
