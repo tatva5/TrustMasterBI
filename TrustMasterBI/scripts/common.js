d532e63c1adff489bea251c4e857d6282963078c
partheshdmehta@gmail.com <partheshdmehta@gmail.com>
2013-02-19 07:29:15 -0600

    add tatva url

--- TrustMasterBI/scripts/common.js
+++ TrustMasterBI/scripts/common.js
@@ -12,7 +12,8 @@ function GetQueryStringParams(sParam, url) {
 }
 
 function callwebservice(controller, method, parameter) {
-	var url = "http://onit1.homenet.org/TrustMasterMobileServices/" + controller + "/" + method;
+    //var url = "http://onit1.homenet.org/TrustMasterMobileServices/" + controller + "/" + method;
+	var url = "http://183.182.91.146/TrustMasterBI/" + controller + "/" + method;
 	if (typeof(parameter)==='undefined')
 		parameter = '';
 	var receivedData;
