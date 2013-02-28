dcbbe13538c4ab27f428745ccc10390e5bd383d1
dharmeshgondhiya@ymail.com <dharmeshgondhiya@ymail.com>
2013-02-28 04:55:51 -0600

    human capital

--- TrustMasterBI/scripts/humancapital.js
+++ TrustMasterBI/scripts/humancapital.js
@@ -1,1 +1,23 @@
-﻿
+function dashboardchart() {
+	//alert("engagement");
+	//$("#countPerCost").kendoChart(result);
+	callwebservice('People', 'TotalEmployeesWithEngagementsAndTermination', '', yearsofservice);
+}
+
+function yearsofservice(result) {
+	alert("yearsofservice");
+	//debugger;
+	$("#union").kendoChart(result);
+	callwebservice('People', 'YearsOfServicePerCostCentrePerCompany', '', Union);
+	//$("#countPerCost").kendoChart(data);
+}
+function Union(result) {
+	$("#engageTermination").kendoChart(result);
+	alert("Union");
+	callwebservice('People', 'UnionRepresentativesPerCompany', '', dashboardComplete);
+}
+function dashboardComplete(result) {
+	$("#yearsofservice").kendoChart(result);
+	//alert("dashboadrd complete");
+	//$("#countPerCost").kendoChart(result);
+}
